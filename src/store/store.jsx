31fbfc7a3741072e88d25068b347616b852f4039
Fadhilkgm <fadhilkgm64@gmail.com>
2022-11-21 23:33:52 +0530

    did some correction configureStore

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,39 +1,40 @@
-import { compose , applyMiddleware} from 'redux';
-import {loggerMiddleware} from './middleware/logger';
-import { persistStore, persistReducer } from 'redux-persist'
-import { rootReducer } from './root-reducer';
-import { configureStore } from '@reduxjs/toolkit';
-import storage from 'redux-persist/lib/storage'
+import { compose, applyMiddleware } from "redux";
+import { loggerMiddleware } from "./middleware/logger";
+import { persistStore, persistReducer } from "redux-persist";
+import { rootReducer } from "./root-reducer";
+import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
 // import { logger } from 'redux-logger';
 //root reducer
 
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
+const middleWares = [
+  process.env.NODE_ENV !== "development" && loggerMiddleware,
+].filter(Boolean);
 
-
-  
-  
-  const persistConfig = { 
-    key: 'root',
-    storage,
-    blacklist: ['user']
-  }
-  const persistedReducer = persistReducer(persistConfig, rootReducer);
-  
-  
-  const middleWares = [process.env.NODE_ENV !== 'production' && loggerMiddleware].filter(
-    Boolean
-  );
-  
-  const composeEnhancer =
-  (process.env.NODE_ENV !== 'production' &&
+const composeEnhancer =
+  (process.env.NODE_ENV !== "development" &&
     window &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
   compose;
-  
-  const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares))
 
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
-  export const store = configureStore({reducer:persistedReducer,middleware:[process.env.NODE_ENV !== 'development' && loggerMiddleware].filter(Boolean)}, undefined, composedEnhancers)
+export const store = configureStore(
+  {
+    reducer: persistedReducer,
+    middleware: [
+     ...middleWares
+    ],
+  },
+  undefined,
+  composedEnhancers
+);
 
-
-  export const persistor = persistStore(store);
+export const persistor = persistStore(store);
